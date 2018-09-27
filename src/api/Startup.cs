68bbd75f578f4ad2c4aadc931ea0ef6ec030303d
Abel Lopes <abellopes@gmail.com>
2018-09-27 12:29:14 -0300

    teste

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -120,7 +120,20 @@ namespace api
             }
             else
             {
-                app.UseHsts();
+               app.UseHsts();
+               app.Use(async (context, next) =>
+               {
+                   await next();
+                   if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("/api/"))
+                   {
+                       context.Request.Path = "/index.html";
+                       await next();
+                   }
+                });
+
+                app.UseMvcWithDefaultRoute();
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
             }
 
             app.UseAuthentication();
